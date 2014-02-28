441298d9e6811904afb34186678a2e090caea80c
Doug Grainger <dhgrainger@gmail.com>
2014-02-28 17:38:31 -0500

    Guess the Number first commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,59 @@
-alert('Hey find me to know where to put the javascript');
+// alert('Hey find me to know where to put the javascript');
+
+
+var random = Math.floor(Math.random()*100)
+console.log(random)
+
+var yourName = " "
+var yourNumber = 0
+var chances = 10
+
+function name () {
+
+yourName = prompt("Please enter your name")
+
+}
+
+
+function number () {
+
+yourNumber = prompt("Enter your number")
+
+output()
+
+}
+
+
+
+function output () {
+	if (chances > 1){
+
+		if (yourNumber == random) {
+			alert("You win " + yourName +"!")
+		}
+		else {
+			if (yourNumber < random) {
+				
+				chances --
+
+				alert("Too low " + yourName + ". Try again. You have " + chances + " Chances!")
+				
+				number()
+			}
+			if (yourNumber > random){
+				
+				chances --
+
+				alert("Too High " + yourName + ". Try again "  + chances + " Chances!")
+				
+				number()
+			}			
+		}
+	}	
+	else {
+		alert("Sorry You Lose")
+	}
+}
+
+name()
+number()
